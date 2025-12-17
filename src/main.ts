1f45a0285237429647062a11823cc9bc6cf68cbf
cleancommits <songpeter428@gmail.com>
2025-12-17 04:20:24 -0800

    Update the informations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'vue3-toastify/dist/index.css'; // Import CSS
+import Vue3Toastify from 'vue3-toastify';
 
 const app = createApp(App);
 
@@ -11,6 +13,11 @@ app.use(pinia);
 import router from './router';
 app.use(router);
 
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+    position: 'top-right',
+    transition: 'slide',
+});
 // main app css
 import './assets/css/app.css';
 
